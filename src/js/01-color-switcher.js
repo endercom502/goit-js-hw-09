baaf9f35fcf81ebc1a56055a6fc889f0bd832300
Andrzej Markiewicz <endercom502@gmail.com>
2022-08-07 20:24:57 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
+let intervalId = null;
+let isActive = false;
 
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  if (!isActive) {
+    intervalId = setInterval(() => {
+      document.body.style.background = getRandomHexColor();
+      isActive = true;
+    }, 1000);
+  }
+}
+function onStopBtnClick() {
+  clearInterval(intervalId);
+  isActive = false;
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const buttonStyle = document.querySelector('[button]');
+buttonStyle.body.p.button.style.color = '#FFFFFF';
