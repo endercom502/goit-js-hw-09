caf298c1de1b1bf46dfd2028c1f79f893a217a1f
endercom502 <99292450+endercom502@users.noreply.github.com>
2022-08-07 18:25:24 +0000

    Deploying to gh-pages from @ endercom502/goit-js-hw-09@baaf9f35fcf81ebc1a56055a6fc889f0bd832300 🚀

---
+++ 01-color-switcher.68897a8f.js
@@ -0,0 +1,2 @@
+!function(){var t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")},n=null,e=!1;t.startBtn.addEventListener("click",(function(){e||(n=setInterval((function(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16)),e=!0}),1e3))})),t.stopBtn.addEventListener("click",(function(){clearInterval(n),e=!1})),document.querySelector("[button]").body.p.button.style.color="#FFFFFF"}();
+//# sourceMappingURL=01-color-switcher.68897a8f.js.map
